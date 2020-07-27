6dc69c41a8047d70ad93551bf4bc8aa47f5fb1a5
RenataNovais <rnovaiscs@gmail.com>
2020-07-27 20:55:04 -0300

    Criado component ProductCard para resolver o problema do codeclimate

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,12 @@
 import React from 'react';
+import ProductCard from './ProductCard';
 
 class ProductList extends React.Component {
   render() {
     const { results } = this.props;
     return (
       <div className="product-list">
-        {results.map((product) => {
-          return (
-            <div data-testid="product" className="product-card" key={product.id}>
-              <p>{product.title}</p>
-              <img src={product.thumbnail} alt={product.title} />
-              <p>{product.price}</p>
-            </div>
-          );
-        })}
+        {results.map((product) => <ProductCard product={product} key={product.id} />)}
       </div>
     );
   }
