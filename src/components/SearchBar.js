fcebcafb02a1115b6a0da287623eddba303f489d
Rafael Assad <rafaassadeng@gmail.com>
2020-07-27 18:33:30 -0300

    req5 a caminho

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,43 @@
 import React from 'react';
-// import getProductsFromCategoryAndQuery from '../services/api';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = { busca: '' };
+    this.buscaProduto = this.buscaProduto.bind(this);
   }
 
+  buscaProduto() {
+    getProductsFromCategoryAndQuery(false, this.state.busca).then((data) => {
+     data.results.map((produto) => {
+    return <div>{ produto.title }</div>
+    });
+  })
+  }
+
+  /*buscaProduto() {
+    const { onClick } =this.props;
+    const { busca } =this.state;
+    if (onClick) 
+    return busca;
+  }*/
+
   render() {
+    const { onClick } = this.props;
+    const { busca } = this.state;
     return (
       <div>
-        <input data-testid="query-input" placeholder="Faça sua busca" />
-        <button data-testid="query-button" type="button">Pesquisar</button>
+        <input
+          gogo={this.buscaProduto}
+          onChange={(event) => this.setState({ busca: event.target.value })}
+          data-testid="query-input"
+          placeholder="Faça sua busca"
+        />
+        <button onClick={this.buscaProduto} data-testid="query-button" type="button">
+        {/*<button onClick={() => onClick(busca)} data-testid="query-button" type="button">*/}
+          Pesquisar
+        </button>
       </div>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CartButton from '../components/CartButton';
 import Categories from '../components/Categories';
+import SearchBar from '../components/SearchBar';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <SearchBar />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
