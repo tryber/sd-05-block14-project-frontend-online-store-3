474fe1d414a024b9fa8bfa9dbec7b85decb5565f
RenataNovais <rnovaiscs@gmail.com>
2020-07-27 15:35:32 -0300

    Criada a página HOME com mensagem inicial e link parapágina do carrinho. Página do Carrinho criada com mensagm inicial

---
+++ src/components/SearchBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import getProductsFromCategoryAndQuery from '../services/api';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      busca: '',
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <input data-testid="query-input" placeholder="Faça sua busca" />
+        <button data-testid="query-button" type="button">Buscar</button>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
