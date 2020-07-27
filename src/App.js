7ae00dca50fe4d2f74908850f13d9ac609f9d741
Rafael Assad <rafaassadeng@gmail.com>
2020-07-27 14:32:17 -0300

    req1 climate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
-import { getProductsFromCategoryAndQuery } from './services/api';
+// import { getProductsFromCategoryAndQuery } from './services/api';
 
 function App() {
   return (
