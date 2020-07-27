8eafcc1b4b79a946d1e076a32eb298395a378ca8
Rafael Assad <rafaassadeng@gmail.com>
2020-07-27 14:26:45 -0300

    requirement1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import { getProductsFromCategoryAndQuery } from './services/api';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route>
+          <Link>asdasdasd</Link>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 export async function getCategories() {
   // implement here
+  const linkAPI = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const getFetch = fetch(linkAPI).then((response) => response.json());
+  return getFetch;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // implement here
+  let linkAPI;
+  if (categoryId && query) linkAPI = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  else if (query) linkAPI = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  else linkAPI = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const getFetch = fetch(linkAPI).then((response) => response.json());
+  return getFetch;
 }
