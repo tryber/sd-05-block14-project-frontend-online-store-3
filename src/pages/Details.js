447b537c8e85efe3ee805697a7372f5cb04c1dd5
Rafael Assad <rafaassadeng@gmail.com>
2020-07-28 09:56:22 -0300

    climate

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -9,9 +9,7 @@ class Details extends React.Component {
 
   componentDidMount() {
     const {
-      match: {
-        params: { id },
-      },
+      match: { params: { id }, },
     } = this.props;
     fetch(`https://api.mercadolibre.com/items/${id}`)
       .then((data) => data.json())
