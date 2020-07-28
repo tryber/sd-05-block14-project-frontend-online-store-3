856b93b750ab03429ba418f088fa50a8ada180fb
RenataNovais <rnovaiscs@gmail.com>
2020-07-28 18:51:43 -0300

    refeito o componente Categories. Definido como radio button - falta implementação do click

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: 0,
+    }
+  }
 
   render() {
+    if (this.state.cart === 0) {
+      return <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+    }
     return (
       <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        <p data-testid="shopping-cart-product-name"></p>
+        <p data-testid="shopping-cart-product-quantity"></p>
       </div>
     );
   }
