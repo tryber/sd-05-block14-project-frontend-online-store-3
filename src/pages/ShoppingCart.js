84ab33049bcbe04b826f2b69428586e7ad489a1b
RenataNovais <rnovaiscs@gmail.com>
2020-07-29 17:14:19 -0300

    tentando arrumar o requisito 3

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CartButton from '../components/CartButton';
 
 class ShoppingCart extends React.Component {
   constructor(props) {
