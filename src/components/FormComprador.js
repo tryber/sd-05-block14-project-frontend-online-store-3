9219eb966f31829e117bbb0497df42abbbd9cf19
Rafael Assad <rafaassadeng@gmail.com>
2020-07-31 09:34:52 -0300

    cc teste forms

--- src/components/FormComprador.js
+++ src/components/FormComprador.js
@@ -25,28 +25,67 @@ class FormComprador extends React.Component {
 
   render() {
     // variavel array que contem os nomes para diminuir o tamanho do input
-    const arr = ['checkout-fullname', 'checkout-cpf', 'checkout-email', 'checkout-phone', 'checkout-cep', 'checkout-address'];
+    const arr = [
+      'checkout-fullname',
+      'checkout-cpf',
+      'checkout-email',
+      'checkout-phone',
+      'checkout-cep',
+      'checkout-address',
+    ];
     return (
       <div>
         <form name="formComprador" method="get">
           <h1>Informações do Comprador</h1>
-          <input data-testid={arr[0]} type="text" 
-          name="fullName" placeholder="Nome" onChange={this.atdad} />
-          <input data-testid={arr[1]} type="number" 
-          name="CPF" placeholder="CPF" onChange={this.atdad} />
-          <input data-testid={arr[2]} type="email" 
-          name="email" placeholder="email" onChange={this.atdad} />
-          <input data-testid={arr[3]} type="number" 
-          name="telefone" placeholder="Tel" onChange={this.atdad} />
-          <input data-testid={arr[4]} type="number" 
-          name="CEP" placeholder="CEP" onChange={this.atdad} />
-          <input data-testid={arr[5]} type="text" 
-          name="endereco" placeholder="End." onChange={this.atdad} />
+          <input
+            data-testid={arr[0]}
+            type="text"
+            name="fullName"
+            placeholder="Nome"
+            onChange={this.atdad}
+          />
+          <input
+            data-testid={arr[1]}
+            type="number"
+            name="CPF"
+            placeholder="CPF"
+            onChange={this.atdad}
+          />
+          <input
+            data-testid={arr[2]}
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={this.atdad}
+          />
+          <input
+            data-testid={arr[3]}
+            type="number"
+            name="telefone"
+            placeholder="Tel"
+            onChange={this.atdad}
+          />
+          <input
+            data-testid={arr[4]}
+            type="number"
+            name="CEP"
+            placeholder="CEP"
+            onChange={this.atdad}
+          />
+          <input
+            data-testid={arr[5]}
+            type="text"
+            name="endereco"
+            placeholder="End."
+            onChange={this.atdad}
+          />
           <input type="text" name="complemento" placeholder="Complemento" onChange={this.atdad} />
           <input type="number" name="numero" placeholder="Número" onChange={this.atdad} />
           <input type="text" name="Cidade" placeholder="Cidade" onChange={this.atdad} />
           <select onChange={this.atdad}>
-            {Estados.map((estado) => <option>{estado}</option>)}
+            {Estados.map((estado) => (
+              <option>{estado}</option>
+            ))}
           </select>
         </form>
       </div>
